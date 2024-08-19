e76d1925cf402df65aa11ba5b9174a924eb59335
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-08-19 15:58:03 +0300

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geolib',
+    version='0.1',
+    packages=find_packages(),
+    description='A geometry library for squares and triangles',
+    author='Ibrahim & Arda',
+    author_email='ibz.04dev@gmail.com' 'cyrek.iota@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
